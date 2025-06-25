931b1e3648849a814f99e296a85f57760eef35cf
delclecianojunior <delclecianoj42@gmail.com>
2025-06-24 22:45:53 -0300

    refatorando o componente de projetos

--- src/app/projetos/projetos.component.ts
+++ src/app/projetos/projetos.component.ts
@@ -1,15 +1,76 @@
-import { ViewportScroller } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projetos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projetos.component.html',
   styleUrl: './projetos.component.css'
 })
 export class ProjetosComponent {
 
-  constructor(private viewportScroller: ViewportScroller) {}
+  projetos = [
+    {
+      titulo: 'Sistema de Cadastro',
+      tecnologias: ['HTML', 'CSS', 'Angular'],
+      imagem: 'imagens/CadastroUsuario.jpg',
+      link: 'https://github.com/DelclecianoSuporte/Cadastro-de-Usuarios' 
+    },
+    {
+      titulo: 'Api Usuários',
+      tecnologias: ['C#', '.NET'],
+      imagem: 'imagens/ApiUsuarios.jpg',
+      link: 'https://github.com/DelclecianoSuporte/Usuarios'
+    },
+    {
+      titulo: 'Pokedex Pokémons',
+      tecnologias: ['HTML', 'CSS', 'Angular'],
+      imagem: 'imagens/pokedex.jpg',
+      link: 'https://github.com/DelclecianoSuporte/angular-pokedex'
+    },
+    {
+      titulo: 'Prime Flix',
+      tecnologias: ['HTML', 'CSS', 'React'],
+      imagem: 'imagens/Prime-Flix.jpg',
+      link: 'https://github.com/DelclecianoSuporte/App-Filmes'
+    },
+    {
+      titulo: 'Api Denúncia',
+      tecnologias: ['C#', '.NET'],
+      imagem: 'imagens/ApiDenuncia.jpg',
+      link: 'https://github.com/DelclecianoSuporte/ApiDenuncia_v2'
+    },
+    {
+      titulo: 'Canal de Denúncia',
+      tecnologias: ['HTML', 'CSS', 'JavaScript'],
+      imagem: 'imagens/Canal-Denuncia.jpg',
+      link: 'https://github.com/DelclecianoSuporte/Site_Denuncia_v2'
+    },
+    {
+      titulo: 'Calculadora IMC',
+      tecnologias: ['HTML','CSS','Angular'],
+      imagem: 'imagens/calculadora-imc.jpg',
+      link: 'https://github.com/DelclecianoSuporte/Calculador-Imc'
+    },
+    {
+      titulo: 'Portfólio',
+      tecnologias: ['HTML', 'CSS', 'Angular'],
+      imagem: 'imagens/Portfolio.jpg',
+      link: 'https://github.com/DelclecianoSuporte/meu-portfolio'
+    },
+    {
+      titulo: 'Gerenciador Financeiro',
+      tecnologias: ['HTML', 'CSS', 'Angular'],
+      imagem: 'imagens/Gerenciador.jpg',
+      link: 'https://github.com/DelclecianoSuporte/Gerenciador-Financeiro'
+    },
+    {
+      titulo: 'Api Gerenciador',
+      tecnologias: ['C#', '.Net'],
+      imagem: 'imagens/ApiGerenciador.jpg',
+      link: 'https://github.com/DelclecianoSuporte/GerenciadorApi'
+    },
+  ]
 
 }
