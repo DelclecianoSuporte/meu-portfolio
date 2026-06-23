95ce4c8e2ee7b51558ff943b6fd6f630b8618bc6
delclecianojunior <delclecianobj@hotmail.com>
2026-06-23 13:06:28 -0300

    Ajustes no cabecalho e rodapé tanto no desktop como no mobile

--- src/app/cabecalho/cabecalho.component.ts
+++ src/app/cabecalho/cabecalho.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-cabecalho',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cabecalho.component.html',
   styleUrl: './cabecalho.component.css'
 })
 export class CabecalhoComponent {
 
-  anoAtual: number = new Date().getFullYear();
+  @Input() rodape = false;
+
+  menuAberto = false;
+
+  anoAtual = new Date().getFullYear();
+
+  toggleMenu() {
+    this.menuAberto = !this.menuAberto;
+  }
 }
